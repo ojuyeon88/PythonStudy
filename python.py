f2bcb08d250bf3c949c014f8bee55f58ec862890
오주연 <129965252+ojuyeon88@users.noreply.github.com>
2024-08-22 23:20:02 +0900

    Update python.py

--- python.py
+++ python.py
@@ -1,14 +1,1 @@
-height = int(input("키를 입력하세요. : "))
-gender = input("성별을 입력하세요. : ")
-
-def std_weight(hegith, gender):
-    std = 0
-    if gender == "남자" :
-        std = hegith/100*hegith/100*22
-    elif gender == "여자" :
-        std = hegith/100*hegith/100*21
-    std = round(std, 2) 
-    return std
-
-cal_std = std_weight(height, gender)
-print("키 {0}cm 남자의 표준 체중은 {1}kg 입니다.".format(height, cal_std))
+print("hello world!")

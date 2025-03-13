d644a5d33d5aeb47da0a20303049a383abbab37c
ojuyeon88 <yexnx01@gmail.com>
2025-03-13 10:55:27 +0900

    Update 1316.py

--- baekjoon/1316.py
+++ baekjoon/1316.py
@@ -1,24 +1,28 @@
-n = int(input())
-
-def solution(n):
-    strings = []
-    for i in range(n):
-        group_input = input()
-        strings.append(group_input)
-    return strings
-
-def group_word(strings):
-    seen = set()
-    prev_char = ''
-
-    for char in strings[0]:
+def is_group_word(word):
+    seen = set()  # 이미 본 문자들을 저장
+    prev_char = ''  # 이전 문자를 저장
+    
+    for char in word:
         if char != prev_char:
             if char in seen:
-                return False
-            seen.add(prev_char)
-        prev_char = char
+                return False  
+            seen.add(prev_char)  
+        prev_char = char 
+    
+    return True 
 
-    return True
+def solution(n, words):
+    count = 0 
+    
+    for word in words:
+        if is_group_word(word):
+            count += 1
+    
+    return count
 
-def end():
-    print()
+# 입력 받기
+n = int(input()) 
+words = [input().strip() for _ in range(n)] 
+
+# 결과 출력
+print(solution(n, words))
